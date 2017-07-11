e4198dc5974fa2d8f4f0547f4cecd06bbf662ebd
Bogdan Orzea <bogdan.orzea@gmail.com>
2017-07-11 13:51:28 +0300

    Added settings for search term

--- app/src/main/java/com/bogdanorzea/newsapp/MainActivity.java
+++ app/src/main/java/com/bogdanorzea/newsapp/MainActivity.java
@@ -1,14 +1,18 @@
 package com.bogdanorzea.newsapp;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
@@ -29,8 +33,15 @@ public class MainActivity extends AppCompatActivity {
             Uri baseUri = Uri.parse(GUARDIAN_BASE_API_LINK);
             Uri.Builder uriBuilder = baseUri.buildUpon();
 
-            // TODO Add user defined search term
-            //uriBuilder.appendQueryParameter("q", "android");
+            // Add user defined search term
+            SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+            String search_term = sharedPrefs.getString(
+                    getString(R.string.settings_search_content_key),
+                    getString(R.string.settings_search_content_default));
+            if (!TextUtils.isEmpty(search_term)) {
+                uriBuilder.appendQueryParameter("q", search_term);
+            }
+
             uriBuilder.appendQueryParameter("order-by", "newest");
             uriBuilder.appendQueryParameter("show-fields", "headline,trailText,thumbnail");
             uriBuilder.appendQueryParameter("show-tags", "contributor");
@@ -113,6 +124,8 @@ public class MainActivity extends AppCompatActivity {
 
         switch (id) {
             case R.id.action_settings:
+                Intent settingsIntent = new Intent(this, SettingsActivity.class);
+                startActivity(settingsIntent);
                 return true;
             case R.id.action_refresh:
                 initializeLoader();
